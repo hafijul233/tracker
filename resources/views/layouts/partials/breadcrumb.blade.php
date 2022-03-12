f149f1a1721fa62a5c02abd58a5d8ee6d1d9c115
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-03-13 01:16:46 +0600

    breadcrumbs hide on sm devices

--- resources/views/layouts/partials/breadcrumb.blade.php
+++ resources/views/layouts/partials/breadcrumb.blade.php
@@ -1,7 +1,7 @@
 @unless ($breadcrumbs->isEmpty())
     <nav aria-label="breadcrumb">
         <h4 class="mb-0">@yield('title')</h4>
-        <ol class="breadcrumb bg-transparent p-0 mb-0">
+        <ol class="breadcrumb bg-transparent p-0 mb-0 d-none d-sm-flex">
             @foreach ($breadcrumbs as $breadcrumb)
 
                 @if ($breadcrumb->url && !$loop->last)
