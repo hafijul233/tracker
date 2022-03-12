6551555a06370ec0fb10ecf661b118817304daa2
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-03-12 11:08:49 +0600

    file style permission added

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,7 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
+
         'upload' => [
             'driver' => 'local',
             'root' => public_path('uploads'),
@@ -59,14 +60,25 @@ return [
             'driver' => 'local',
             'root'   => public_path('media'),
             'url'    => env('APP_URL').'/media',
+            'visibility' => 'public',
         ],
 
         'avatar' => [
             'driver' => 'local',
             'root'   => public_path('media/avatars'),
             'url'    => env('APP_URL').'/media/avatars',
+            'visibility' => 'public',
+            'permissions' => [
+                'file' => [
+                    'public' => 0774,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0700,
+                ],
         ],
-
+        ],
 
         's3' => [
             'driver' => 's3',
