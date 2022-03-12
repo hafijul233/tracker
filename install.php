dc9ac8990a0c9e59f8799277f05fff57530b4b11
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-03-13 02:08:26 +0600

    installer added

---
+++ install.php
@@ -0,0 +1,8 @@
+<pre>
+<?php
+var_dump(exec("php -v"));
+var_dump(ini_set('max_execution_time', "2100"));
+var_dump(exec("php composer.phar install"));
+var_dump(exec("php -r \"file_exists('.htaccess') || copy('.htaccess.example', '.htaccess');\""));
+var_dump(exec("php -r \"file_exists('public/.htaccess') || copy('public/.htaccess.example', 'public/.htaccess');\""));
+var_dump(exec("php artisan key:generate"));
